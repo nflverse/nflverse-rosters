6efb951dba44bbb092940c2ddf045483bf46ad9f
mrcaseb <roemerle@gmail.com>
2021-08-04 22:49:12 +0200

    rename script for consistency

--- R/update-depth-charts.R
+++ R/update-depth-charts.R
@@ -1,6 +1,6 @@
-seasons_to_scrape <- c(2001:(lubridate::year(Sys.Date())))
+seasons_to_scrape <- 2001:nflreadr:::most_recent_season()
 
-message('Scraping teams...')
+cli::cli_alert_info("Scraping teams...")
 
 scrape_teams <- function(year) {
   h <- httr::handle("https://www.nfl.info")
@@ -20,22 +20,23 @@ scrape_teams <- function(year) {
 }
 
 teams <- purrr::map_dfr(seasons_to_scrape, scrape_teams) |>
-  dplyr::filter(!(ClubCode %in% c('AFC', 'NFC', 'RIC', 'SAN', 'CRT', 'IRV'))) |> # remove all-star teams
+  dplyr::filter(!(ClubCode %in% c("AFC", "NFC", "RIC", "SAN", "CRT", "IRV"))) |>
+  # remove all-star teams
   dplyr::mutate(Season = as.integer(Season)) |>
   dplyr::select(club_code = ClubCode, season = Season) |>
-  tidyr::expand_grid(season_type = c('REG', 'POST'))
+  tidyr::expand_grid(season_type = c("REG", "POST"))
 
-message('Scraping depth charts...')
+cli::cli_alert_info("Scraping depth charts...")
 
 scrape_dc <- function(season, team, season_type) {
   cli::cli_process_start("Loading {season} {team}, {season_type}")
-  h <- httr::handle('https://www.nfl.info')
+  h <- httr::handle("https://www.nfl.info")
   r <- httr::GET(
     handle = h,
     path = glue::glue(
-      '/nfldataexchange/dataexchange.asmx/getGameDepthChart?lSeason={season}&lSeasonType={season_type}&lWeek=0&lClub={team}'
+      "/nfldataexchange/dataexchange.asmx/getGameDepthChart?lSeason={season}&lSeasonType={season_type}&lWeek=0&lClub={team}"
     ),
-    httr::authenticate('media', 'media'),
+    httr::authenticate("media", "media"),
     url = NULL
   )
   dc_df <- httr::content(r) |>
@@ -47,20 +48,18 @@ scrape_dc <- function(season, team, season_type) {
 }
 
 dc_df <-
-  purrr::pmap_dfr(list(teams$season, teams$club_code, teams$season_type),
-                  scrape_dc) |>
+  purrr::pmap_dfr(list(teams$season, teams$club_code, teams$season_type), scrape_dc) |>
   dplyr::mutate(
     ClubCode = dplyr::case_when(
-      ClubCode == 'ARZ' ~ 'ARI',
-      ClubCode == 'BLT' ~ 'BAL',
-      ClubCode == 'CLV' ~ 'CLE',
-      ClubCode == 'HST' ~ 'HOU',
-      ClubCode == 'SL' ~ 'STL',
+      ClubCode == "ARZ" ~ "ARI",
+      ClubCode == "BLT" ~ "BAL",
+      ClubCode == "CLV" ~ "CLE",
+      ClubCode == "HST" ~ "HOU",
+      ClubCode == "SL" ~ "STL",
       T ~ ClubCode
     ),
     full_name = paste(FootballName, LastName),
-    across(c(Season, Week, DepthTeam, JerseyNumber),
-           as.integer)
+    dplyr::across(c(Season, Week, DepthTeam, JerseyNumber), as.integer)
   ) |>
   dplyr::select(
     season = Season,
@@ -78,31 +77,27 @@ dc_df <-
     gsis_id = GsisID
   )
 
-message('Saving depth charts...')
+cli::cli_alert_info("Saving depth charts...")
 dc_split <- dc_df |>
   dplyr::group_split(season)
 
 purrr::walk(dc_split, function(x) {
-  saveRDS(x, glue::glue('data/seasons/dc_{unique(x$season)}.rds'))
-  readr::write_csv(x,
-                   glue::glue('data/seasons/dc_{unique(x$season)}.csv.gz'))
-  qs::qsave(
-    x,
-    glue::glue('data/seasons/dc_{unique(x$season)}.qs'),
-    preset = "custom",
-    algorithm = "zstd_stream",
-    compress_level = 22,
-    shuffle_control = 15
-  )
+  saveRDS(x, glue::glue("data/seasons/dc_{unique(x$season)}.rds"))
+  readr::write_csv(x, glue::glue("data/seasons/dc_{unique(x$season)}.csv.gz"))
 })
 
-saveRDS(dc_df, 'data/nflfastR-dc.rds')
-readr::write_csv(dc_df, 'data/nflfastR-dc.csv.gz')
+full_dc_df <- list.files("data/seasons", pattern = "dc_[0-9]+\\.rds", full.names = TRUE) |>
+  purrr::map_dfr(readRDS)
+
+saveRDS(full_dc_df, "data/nflfastR-dc.rds")
+readr::write_csv(full_dc_df, "data/nflfastR-dc.csv.gz")
 qs::qsave(
-  dc_df,
-  'data/nflfastR-dc.qs',
+  full_dc_df,
+  "data/nflfastR-dc.qs",
   preset = "custom",
   algorithm = "zstd_stream",
   compress_level = 22,
   shuffle_control = 15
 )
+
+cli::cli_alert_success("Finished scraping depth charts!")
