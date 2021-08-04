551a742b32f5bbea8decbc4433c0047792de8001
mrcaseb <roemerle@gmail.com>
2021-08-04 21:34:54 +0200

    remove irrelevant files

--- R/git.R
+++
@@ -1,6 +0,0 @@
-git <- function(..., echo_cmd = TRUE, echo = TRUE, error_on_status = TRUE) {
-  callr::run("git", c(...),
-             echo_cmd = echo_cmd, echo = echo,
-             error_on_status = error_on_status
-  )
-}
--- R/make_roster_commit.R
+++
@@ -1,3 +0,0 @@
-source("R/git.R")
-message <- sprintf("Roster updated %s (ET)", lubridate::now("America/New_York"))
-git("commit", "-am", message)
