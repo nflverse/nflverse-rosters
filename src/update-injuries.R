d17bc59d5a695f33886e398a45146f2058bdfcae
John Edwards <edwards1860@gmail.com>
2022-07-25 22:27:50 -0700

    complete rework to resolve #38

--- src/update-injuries.R
+++ src/update-injuries.R
@@ -1,100 +1,108 @@
-most_rec_season <- stringi::stri_extract_all_regex(dir("data/seasons"), "injuries_[0-9]{4}") |>
-  unlist() |>
-  na.omit() |>
-  max() |>
-  (\(x)gsub("injuries_", "", x))()
+scrape_ir <- function(year, week, game_type) {
+  h <- httr::handle("https://www.nfl.info")
+  on.exit(rm(h), add = TRUE) # close handle when function exits
 
-most_rec_season <- ifelse(is.na(most_rec_season), 2009, most_rec_season)
+  r <- try({
+    httr::GET(
+      handle = h,
+      path = glue::glue(
+        "/nfldataexchange/dataexchange.asmx/getInjuryData?lseason={year}&lweek={week}&lseasontype={game_type}"
+      ),
+      httr::authenticate("media", "media"),
+      url = NULL
+    ) |>
+      httr::content() |>
+      XML::xmlParse() |>
+      XML::xmlToDataFrame()
+  }, silent = TRUE)
 
-# most_rec_season <- 2009
-
-cli::cli_alert_info("Fetching schedule...")
-
-weeks <- nflreadr::load_schedules() |>
-  dplyr::mutate(game_type = dplyr::case_when(game_type == 'REG' ~ 'REG',
-                                             game_type %in% c('WC','DIV','CON','SB') ~ 'POST',
-                                             T~NA_character_),
-                game_type = factor(game_type,levels=c('REG','POST')),
-                week = dplyr::case_when(game_type == 'REG' ~ as.integer(week),
-                                        T ~ as.integer(week - max(week[game_type=='REG'])))) |>
-  dplyr::filter(season >= most_rec_season) |>
-  dplyr::ungroup() |>
-  dplyr::group_by(season, game_type, week) |>
-  dplyr::summarise(games_played = sum(!is.na(result)),.groups="keep") |>
-  dplyr::ungroup() |>
-  dplyr::mutate(filter_week = which(games_played == 0)[1]) |>
-  dplyr::filter(dplyr::row_number() <= filter_week) |>
-  dplyr::select(c(season, game_type = game_type, week))
-
-if(nrow(weeks) > 0){
-  cli::cli_alert_info("Scraping injury reports...")
-
-  scrape_ir <- function(year, week, game_type) {
-    cli::cli_process_start("Loading {year}, {game_type} Week {week}", on_exit = "done")
-
-    h <- httr::handle("https://www.nfl.info")
-    on.exit(rm(h), add = TRUE) # close handle when function exits
-
-    r <- try({
-      httr::GET(
-        handle = h,
-        path = glue::glue(
-          "/nfldataexchange/dataexchange.asmx/getInjuryData?lseason={year}&lweek={week}&lseasontype={game_type}"
-        ),
-        httr::authenticate("media", "media"),
-        url = NULL) |>
-        httr::content() |>
-        XML::xmlParse() |>
-        XML::xmlToDataFrame()
-    }, silent = TRUE)
-
-    if (inherits(r, "try-error")) {
-      cli::cli_process_failed()
-      return(data.frame())
-    }
-
-    return(r)
+  if (inherits(r, "try-error")) {
+    return(data.frame())
   }
 
-  ir_df <-
-    purrr::pmap_dfr(list(weeks$season, weeks$week, weeks$game_type), scrape_ir) |>
-    dplyr::mutate(
-      ClubCode = dplyr::case_when(
-        ClubCode == "ARZ" ~ "ARI",
-        ClubCode == "BLT" ~ "BAL",
-        ClubCode == "CLV" ~ "CLE",
-        ClubCode == "HST" ~ "HOU",
-        ClubCode == "SL" ~ "STL",
-        T ~ ClubCode
-      ),
-      full_name = paste(FootballName, LastName),
-      date_modified = lubridate::as_datetime(ModifiedDt, format = "%s"),
-      dplyr::across(
-        c(Injury1:InjuryStatus, PracticeStatus:Practice2),
-        ~ dplyr::case_when(
-          .x == "--" ~ NA_character_,
-          T ~ .x
-        )
-      )
-    ) |>
-    dplyr::select(
-      season = Season,
-      season_type = SeasonType,
-      team = ClubCode,
-      week = Week,
-      gsis_id = GsisID,
-      position = Position,
-      full_name,
-      first_name = FootballName,
-      last_name = LastName,
-      report_primary_injury = Injury1,
-      report_secondary_injury = Injury2,
-      report_status = InjuryStatus,
-      practice_primary_injury = Practice1,
-      practice_secondary_injury = Practice2,
-      practice_status = PracticeStatus,
-      date_modified
+  return(r)
+}
+
+build_ir <- function(season) {
+  cli::cli_alert_info("Building injury reports...")
+
+  weeks <- expand.grid(
+    season = c(season),
+    week = c(1:18),
+    game_type = c("REG", "POST")
+  ) |>
+    dplyr::filter(
+      (season <= 2020 & week <= 17 & game_type == "REG") |
+        (season > 2020 & game_type == "REG") |
+        (game_type == "POST" & week <= 5)
     )
+
+  progressr::with_progress({
+    p <- progressr::progressor(steps = nrow(weeks))
+
+    ir_df <-
+      purrr::pmap_dfr(list(weeks$season, weeks$week, weeks$game_type), \(x, y, z) {
+        df <- scrape_ir(x, y, z)
+        p()
+        return(df)
+      }) |>
+      dplyr::mutate(
+        ClubCode = dplyr::case_when(
+          ClubCode == "ARZ" ~ "ARI",
+          ClubCode == "BLT" ~ "BAL",
+          ClubCode == "CLV" ~ "CLE",
+          ClubCode == "HST" ~ "HOU",
+          ClubCode == "SL" ~ "STL",
+          T ~ ClubCode
+        ),
+        full_name = paste(FootballName, LastName),
+        date_modified = lubridate::as_datetime(ModifiedDt, format = "%s"),
+        dplyr::across(
+          c(Injury1:InjuryStatus, PracticeStatus:Practice2),
+          ~ dplyr::case_when(.x == "--" ~ NA_character_,
+                             T ~ .x)
+        ),
+        Season = as.numeric(Season),
+        Week = as.numeric(Week)
+      ) |>
+      dplyr::select(
+        season = Season,
+        game_type = SeasonType,
+        team = ClubCode,
+        week = Week,
+        gsis_id = GsisID,
+        position = Position,
+        full_name,
+        first_name = FootballName,
+        last_name = LastName,
+        report_primary_injury = Injury1,
+        report_secondary_injury = Injury2,
+        report_status = InjuryStatus,
+        practice_primary_injury = Practice1,
+        practice_secondary_injury = Practice2,
+        practice_status = PracticeStatus,
+        date_modified
+      ) |>
+      dplyr::group_by(season) |>
+      dplyr::mutate(
+        game_type = dplyr::case_when(
+          game_type == "POST" & week == 1 ~ "WC",
+          game_type == "POST" &
+            week == 2 ~ "DIV",
+          game_type == "POST" &
+            week == 3 ~ "CON",
+          game_type == "POST" &
+            week == 4 ~ "SB",
+          T ~ game_type
+        ),
+        week = dplyr::case_when(
+          game_type %in% c("WC", "DIV", "CON", "SB") ~ week + max(week[game_type == "REG"]),
+          T ~ week
+        ),
+
+      ) |>
+      dplyr::ungroup()
+  })
 
   cli::cli_alert_info("Saving injury reports...")
 
@@ -102,33 +110,17 @@ if(nrow(weeks) > 0){
     dplyr::group_split(season)
 
   purrr::walk(ir_split, function(x) {
-
     nflversedata::nflverse_save(
       data_frame = x,
-      file_name = paste0("injuries_",unique(x$season)),
+      file_name = paste0("injuries_", unique(x$season)),
       nflverse_type = "injury & practice reports",
-      release_tag = "injuries")
-
-    # saveRDS(x, glue::glue("data/seasons/injuries_{unique(x$season)}.rds"))
-    # readr::write_csv(x, glue::glue("data/seasons/injuries_{unique(x$season)}.csv.gz"))
+      release_tag = "injuries"
+    )
   })
-
-  # full_ir_df <- list.files("data/seasons", pattern = "injuries_[0-9]+\\.rds", full.names = TRUE) |>
-  #   purrr::map_dfr(readRDS)
-  #
-  # saveRDS(full_ir_df, "data/nflfastR-injuries.rds")
-  # readr::write_csv(full_ir_df, "data/nflfastR-injuries.csv.gz")
-  # qs::qsave(
-  #   full_ir_df,
-  #   "data/nflfastR-injuries.qs",
-  #   preset = "custom",
-  #   algorithm = "zstd_stream",
-  #   compress_level = 22,
-  #   shuffle_control = 15
-  # )
-  cli::cli_alert_success("Finished scraping injuries!")
-} else {
-  cli::cli_alert_warning("Nothing to load. It's probably offseason.")
 }
 
+# build all seasons
+# build_ir(2009:nflreadr:::most_recent_season())
 
+# update most recent season
+build_ir(nflreadr:::most_recent_season())
