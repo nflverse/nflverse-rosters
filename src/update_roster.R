93ccb2686242a3155ed6fa7b497890aedae04203
John Edwards <edwards1860@gmail.com>
2023-03-20 12:08:28 -0700

    Update update_roster.R
    fixes issue where update_rosters would fail in attempting to fetch rosters where season has not been updated in the NGS API

--- src/update_roster.R
+++ src/update_roster.R
@@ -195,75 +195,81 @@ build_rosters_weekly_dataexchange <- function(season) {
 build_rosters_weekly_ngsapi <- function(season) {
   cli::cli_alert_info("Using NGS API to obtain roster information!")
   cli::cli_alert_info("Scraping team and weeks for {season}...")
-  teams <- ngsscrapR::scrape_teams(season) |>
-    dplyr::filter(!is.na(division_id))
-  weeks <-
-    purrr::map_dfr(c("REG", "POST"),
-                   ~ ngsscrapR::scrape_schedule(season, .x)) |>
-    dplyr::group_by(week, game_type) |>
-    dplyr::summarise(.groups = "drop")
-  team_week_pair <- teams |>
-    dplyr::select(team_id) |>
-    dplyr::mutate(join = 1) |>
-    dplyr::left_join(weeks |>
-                       dplyr::mutate(join = 1), by = c("join")) |>
-    dplyr::select(-c(join)) |>
-    dplyr::rename(teamId = team_id,
-                  seasonType = game_type) |>
-    dplyr::mutate(
-      season = !!season,
-      seasonType = dplyr::case_when(seasonType != "REG" ~ "POST",
-                                    T ~ seasonType)
-    )
-  cli::cli_alert_info("Scraping rosters for {season}...")
-  scrape_rosters <- function() {
-    p <- progressr::progressor(along = 1:nrow(team_week_pair))
-    weekly_rosters <-
-      purrr::pmap_dfr(
-        list(
-          team_week_pair$teamId,
-          team_week_pair$season,
-          team_week_pair$seasonType,
-          team_week_pair$week
-        ),
-        \(w, x, y, z) {
-          roster <- ngsscrapR::scrape_roster(w, x, y, z)
-          p()
-          return(roster)
-        }
-      )
-  }
-
-  progressr::with_progress({
-    weekly_rosters <- scrape_rosters() |>
-      dplyr::mutate(years_exp = as.integer(season) - as.integer(entry_year)) |>
-      dplyr::rename(
-        position = position_group,
-        depth_chart_position = position,
-        game_type = season_type
-      ) |>
-      dplyr::group_by(game_type) |>
-      dplyr::mutate(week = dplyr::dense_rank(week)) |> # fixing some weirdness where we skip a week
-      dplyr::ungroup() |>
+  teams <- ngsscrapR::scrape_teams(season)
+  if(length(teams)) {
+    teams <- teams |>
+      dplyr::filter(!is.na(division_id))
+    weeks <-
+      purrr::map_dfr(c("REG", "POST"),
+                     ~ ngsscrapR::scrape_schedule(season, .x)) |>
+      dplyr::group_by(week, game_type) |>
+      dplyr::summarise(.groups = "drop")
+    team_week_pair <- teams |>
+      dplyr::select(team_id) |>
+      dplyr::mutate(join = 1) |>
+      dplyr::left_join(weeks |>
+                         dplyr::mutate(join = 1), by = c("join")) |>
+      dplyr::select(-c(join)) |>
+      dplyr::rename(teamId = team_id,
+                    seasonType = game_type) |>
       dplyr::mutate(
-        game_type = dplyr::case_when(
-          game_type == "POST" & week == 1 ~ "WC",
-          game_type == "POST" &
-            week == 2 ~ "DIV",
-          game_type == "POST" &
-            week == 3 ~ "CON",
-          game_type == "POST" &
-            week == 4 ~ "SB",
-          T ~ game_type
-        ),
-        week = dplyr::case_when(
-          game_type %in% c("WC", "DIV", "CON", "SB") ~ week + max(week[game_type == "REG"]),
-          T ~ week
-        ),
+        season = !!season,
+        seasonType = dplyr::case_when(seasonType != "REG" ~ "POST",
+                                      T ~ seasonType)
       )
-    weekly_rosters[["birth_date"]] <-
-      NULL # sometimes ngsscrapR::scrape_rosters() returns this, sometimes it doesn't
-  })
+    cli::cli_alert_info("Scraping rosters for {season}...")
+    scrape_rosters <- function() {
+      p <- progressr::progressor(along = 1:nrow(team_week_pair))
+      weekly_rosters <-
+        purrr::pmap_dfr(
+          list(
+            team_week_pair$teamId,
+            team_week_pair$season,
+            team_week_pair$seasonType,
+            team_week_pair$week
+          ),
+          \(w, x, y, z) {
+            roster <- ngsscrapR::scrape_roster(w, x, y, z)
+            p()
+            return(roster)
+          }
+        )
+    }
+
+    progressr::with_progress({
+      weekly_rosters <- scrape_rosters() |>
+        dplyr::mutate(years_exp = as.integer(season) - as.integer(entry_year)) |>
+        dplyr::rename(
+          position = position_group,
+          depth_chart_position = position,
+          game_type = season_type
+        ) |>
+        dplyr::group_by(game_type) |>
+        dplyr::mutate(week = dplyr::dense_rank(week)) |> # fixing some weirdness where we skip a week
+        dplyr::ungroup() |>
+        dplyr::mutate(
+          game_type = dplyr::case_when(
+            game_type == "POST" & week == 1 ~ "WC",
+            game_type == "POST" &
+              week == 2 ~ "DIV",
+            game_type == "POST" &
+              week == 3 ~ "CON",
+            game_type == "POST" &
+              week == 4 ~ "SB",
+            T ~ game_type
+          ),
+          week = dplyr::case_when(
+            game_type %in% c("WC", "DIV", "CON", "SB") ~ week + max(week[game_type == "REG"]),
+            T ~ week
+          ),
+        )
+      weekly_rosters[["birth_date"]] <-
+        NULL # sometimes ngsscrapR::scrape_rosters() returns this, sometimes it doesn't
+    })
+  } else {
+    cli::cli_alert_warning("No rosters found for {season}! It is possible the league year has not flipped over yet.")
+    weekly_rosters <- NULL
+  }
 
   return(weekly_rosters)
 
@@ -400,49 +406,55 @@ build_rosters <-
 
     if (season >= 2016) {
       weekly_rosters <- build_rosters_weekly_ngsapi(season)
-      roster <- convert_weekly_to_season_rosters(weekly_rosters)
-      shield <- build_rosters_shieldapi(season)
-      weekly_rosters <- weekly_rosters |>
-        dplyr::select(-c(height)) |>
-        dplyr::left_join(
-          shield |>
-            dplyr::select(gsis_id, birth_date, height) |>
-            dplyr::distinct(),
-          by = c("gsis_id"),
-          na_matches = "never"
-        ) |>
-        fill_ids()
-      roster <- roster |>
-        dplyr::select(-c(height)) |>
-        dplyr::left_join(
-          shield |>
-            dplyr::select(gsis_id, birth_date, height) |>
-            dplyr::distinct(),
-          by = c("gsis_id"),
-          na_matches = "never"
-        ) |>
-        fill_ids()
+      if(length(weekly_rosters)) {
+        roster <- convert_weekly_to_season_rosters(weekly_rosters)
+        shield <- build_rosters_shieldapi(season)
+        weekly_rosters <- weekly_rosters |>
+          dplyr::select(-c(height)) |>
+          dplyr::left_join(
+            shield |>
+              dplyr::select(gsis_id, birth_date, height) |>
+              dplyr::distinct(),
+            by = c("gsis_id"),
+            na_matches = "never"
+          ) |>
+          fill_ids()
+        roster <- roster |>
+          dplyr::select(-c(height)) |>
+          dplyr::left_join(
+            shield |>
+              dplyr::select(gsis_id, birth_date, height) |>
+              dplyr::distinct(),
+            by = c("gsis_id"),
+            na_matches = "never"
+          ) |>
+          fill_ids()
+      }
+
     }
 
-    if (season >= 2002) {
-      cli::cli_alert_info("Save weekly rosters...")
+    if (length(weekly_rosters)) {
+      if (season >= 2002) {
+        cli::cli_alert_info("Save weekly rosters...")
+        nflversedata::nflverse_save(
+          data_frame = weekly_rosters,
+          file_name =  glue::glue("roster_weekly_{season}"),
+          nflverse_type = "weekly roster data",
+          release_tag = "weekly_rosters"
+        )
+      }
+
+      cli::cli_alert_info("Save season rosters...")
       nflversedata::nflverse_save(
-        data_frame = weekly_rosters,
-        file_name =  glue::glue("roster_weekly_{season}"),
-        nflverse_type = "weekly roster data",
-        release_tag = "weekly_rosters"
+        data_frame = roster,
+        file_name =  glue::glue("roster_{season}"),
+        nflverse_type = "roster data",
+        release_tag = "rosters"
       )
+
+      cli::cli_alert_info("DONE!")
     }
 
-    cli::cli_alert_info("Save season rosters...")
-    nflversedata::nflverse_save(
-      data_frame = roster,
-      file_name =  glue::glue("roster_{season}"),
-      nflverse_type = "roster data",
-      release_tag = "rosters"
-    )
-
-    cli::cli_alert_info("DONE!")
 
     return(invisible(NULL))
   }
